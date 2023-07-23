3e45d2364b3d7b470fcdea07f26b5b821560b3c2
MertArar <mertarar.ma@gmail.com>
2023-07-24 02:42:02 +0300

    header changed

--- src/components/pages/Pages.jsx
+++ src/components/pages/Pages.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Header from "../common/header/Header";
 import Home from "../home/Home";
 import About from "../about/About";
 import Footer from "../common/footer/Footer";
+import Navbar from "../navbar/Navbar";
 
 const Pages = () => {
   return (
     <>
       <BrowserRouter>
-        <Header />
+        <Navbar />
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/about" element={<About />} />
