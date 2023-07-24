d6207c834b74a7d842b6cb447e35086cc796d105
MertArar <85193329+MertArar@users.noreply.github.com>
2023-07-24 10:47:25 +0300

    some chnages added

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import HeroVideo from "./heroVideo/heroVideo";
+import About from "../about/About";
 
 const Home = () => {
   return (
     <div>
       <HeroVideo />
+      <About />
     </div>
   );
 };
