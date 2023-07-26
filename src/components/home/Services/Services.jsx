bf4548a26e5c5bbfc2e790d2cb429f4903c07123
MertArar <85193329+MertArar@users.noreply.github.com>
2023-07-26 04:40:36 +0300

    services addedd

--- src/components/home/Services/Services.jsx
+++ src/components/home/Services/Services.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import "./Services.css";
+
+import { AiOutlineArrowRight } from "react-icons/ai";
 
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <div className="services-container">
+      <div className="services-left">
+        <h2>What Can We Do?</h2>
+        <h1>Services we can help you with</h1>
+        <span>
+          See All Services <AiOutlineArrowRight />{" "}
+        </span>
+      </div>
+      <div className="services-right">
+        <div className="card">
+          <div className="card-inner">
+            <img src="" alt="" />
+          </div>
+          <div className="card-inner"></div>
+          <div className="card-inner"></div>
+          <div className="card-inner"></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Services;
